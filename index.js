56e408f2b0e359c2f3c1bc4a6d83aca3d673417d
jterry <jordan.terry@mx.com>
2019-04-05 12:00:13 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+/**
+ * @format
+ */
+
+// import {AppRegistry} from 'react-native';
+// import App from './App';
+// import {name as appName} from './app.json';
+
+// AppRegistry.registerComponent(appName, () => App);
+
+import React from 'react';
+import { AppRegistry } from 'react-native';
+import Header from './src/components/header';
+
+const App = () => {
+    return (
+       <Header headerText={'Albums'} />
+       
+    );
+};
+
+AppRegistry.registerComponent('albums', () => App);
