38067e4a30ad62beb33b14ba39cbc4108f937a01
jterry <jordan.terry@mx.com>
2019-04-05 15:51:15 -0600

    added styling to cards

--- src/components/albumDetail.js
+++ src/components/albumDetail.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Text } from 'react-native';
 import Card from './card';
+import CardSection from './cardSection';
 
 const AlbumDetail = (props) => {
     return (
         <Card>
-            <Text>{props.album.title}</Text>
+            <CardSection>
+                <Text>{props.album.title}</Text>
+            </CardSection>
         </Card>
     );
 };
